164f4d3db1b1481a14db0037d6d3531764015cab
mpascualach <mpascualacheson1@gmail.com>
2024-04-08 18:52:11 +0200

    flexboxed the page a little

--- app/page.tsx
+++ app/page.tsx
@@ -26,104 +26,128 @@ export default function Home() {
   const [style, setStyle] = useState("vivid");
 
   return (
-    <div className="container mx-auto px-4 py-8 h-lvh">
-      <h1 className="text-4xl font-bold mb-8">I want an image</h1>
-      <p>I want a
-        <select
-          id="genre"
-          name="genre"
-          value={genre}
-          className="p-2 m-2 bg-transparent border rounded-md bg-blue-100"
-          onChange={(e) => setGenre(e.target.value)}
-        >
-          <option key="" value="">…</option>
-          {genres.map((g) => <option key={g} value={g} className="text-black">{g}</option>)}
-        </select>
-      </p>
-      {genre && <p>
-        …of a
-        <select
-          id="subject"
-          name="subject"
-          value={subject}
-          className="p-2 m-2 bg-transparent border rounded-md bg-blue-100"
-          onChange={(e) => setSubject(e.target.value)}
-        >
-          <option key="" value="">…</option>
-          {subjects.map((s) => <option key={s} value={s} className="text-black">{s}</option>)}
-        </select>
-      </p>}
-      {genre && subject && <p>
-        …with a
-        <select
-          id="accessory"
-          name="accessory"
-          value={accessory}
-          className="p-2 m-2 bg-transparent border rounded-md bg-blue-100"
-          onChange={(e) => setAccessory(e.target.value)}
-        >
-          <option key="" value="">…</option>
-          {accessories.map((a) => <option key={a} value={a} className="text-black">{a}</option>)}
-        </select>
-      </p>}
-      {genre && subject && <p>
-        …
-        <select
-          id="activity"
-          name="activity"
-          value={activity}
-          className="p-2 m-2 bg-transparent border rounded-md bg-blue-100"
-          onChange={(e) => setActivity(e.target.value)}
-        >
-          <option key="" value="">…</option>
-          {activities.map((a) => <option key={a} value={a} className="text-black">{a}</option>)}
-        </select>
-      </p>}
-      {genre && subject && <p>
-        …on a
-        <select
-          id="location"
-          name="location"
-          value={location}
-          className="p-2 m-2 bg-transparent border rounded-md bg-blue-100"
-          onChange={(e) => setLocation(e.target.value)}
-        >
-          <option key="" value="">…</option>
-          {locations.map((l) => <option key={l} value={l} className="text-black">{l}</option>)}
-        </select>
-      </p>}
-      {genre && subject && <button
-        className="p-4 m-2 border rounded-md shadow-sm font-medium bg-green-300"
-        disabled={isPromptLoading || isImageLoading}
-        onClick={(e) => {
-          e.preventDefault();
-          setGenre(genres[Math.floor(genres.length*Math.random())]);
-          setSubject(subjects[Math.floor(subjects.length*Math.random())]);
-          setAccessory(accessories[Math.floor(accessories.length*Math.random())]);
-          setActivity(activities[Math.floor(activities.length*Math.random())]);
-          setLocation(locations[Math.floor(locations.length*Math.random())]);
-        }}
-      >🎲</button>}
-      {genre && subject && <button
-        className="p-4 m-2 border rounded-md shadow-sm font-medium bg-green-300"
-        disabled={isPromptLoading || isImageLoading}
-        onClick={async (e) => {
-          e.preventDefault();
-          try {
-            setPromptLoading(true);
-            const response = await fetch("api/prompt", {
-              method: 'POST',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify({genre, subject, accessory, activity, location}),
-            });
-            const data = await response.json();
-            setPrompt(data.prompt);
-          } finally {
-            setPromptLoading(false);
-          }
-        }}
-      >{isPromptLoading ? "Generating…" : "Generate prompt"}</button>}
-      {genre && subject && prompt && <p><textarea value={prompt} className="w-full h-32"></textarea></p>}
+    <div className="container mx-auto px-4 py-8 h-lvh flex flex-col items-center">
+      <h1 className="text-4xl font-bold mb-8">Let me make art for you!</h1>
+
+      <div className="flex">
+        {/* Art Style / genre */}
+        <p>I want a
+          <select
+            id="genre"
+            name="genre"
+            value={genre}
+            className="p-2 m-2 bg-transparent border rounded-md bg-blue-100"
+            onChange={(e) => setGenre(e.target.value)}
+          >
+            <option key="" value="">…</option>
+            {genres.map((g) => <option key={g} value={g} className="text-black">{g}</option>)}
+          </select>
+        </p>
+
+        {/* Subject */}
+        {genre && <p>
+          of a
+          <select
+            id="subject"
+            name="subject"
+            value={subject}
+            className="p-2 m-2 bg-transparent border rounded-md bg-blue-100"
+            onChange={(e) => setSubject(e.target.value)}
+          >
+            <option key="" value="">…</option>
+            {subjects.map((s) => <option key={s} value={s} className="text-black">{s}</option>)}
+          </select>
+        </p>}
+
+        {/* Accessory */}
+        {genre && subject && <p>
+          with a
+          <select
+            id="accessory"
+            name="accessory"
+            value={accessory}
+            className="p-2 m-2 bg-transparent border rounded-md bg-blue-100"
+            onChange={(e) => setAccessory(e.target.value)}
+          >
+            <option key="" value="">…</option>
+            {accessories.map((a) => <option key={a} value={a} className="text-black">{a}</option>)}
+          </select>
+        </p>}
+      </div>
+
+      <div className="flex">
+
+        {/* Doing something... (activity) */}
+        {genre && subject && <p>
+          <select
+            id="activity"
+            name="activity"
+            value={activity}
+            className="p-2 m-2 bg-transparent border rounded-md bg-blue-100"
+            onChange={(e) => setActivity(e.target.value)}
+          >
+            <option key="" value="">…</option>
+            {activities.map((a) => <option key={a} value={a} className="text-black">{a}</option>)}
+          </select>
+        </p>}
+
+        {/* At a location */}
+        {genre && subject && <p>
+          on a
+          <select
+            id="location"
+            name="location"
+            value={location}
+            className="p-2 m-2 bg-transparent border rounded-md bg-blue-100"
+            onChange={(e) => setLocation(e.target.value)}
+          >
+            <option key="" value="">…</option>
+            {locations.map((l) => <option key={l} value={l} className="text-black">{l}</option>)}
+          </select>
+        </p>}
+      </div>
+
+      <div className="flex gap-4">
+        {genre && subject && <button
+          className="p-4 m-2 border rounded-md shadow-sm font-medium bg-green-300"
+          disabled={isPromptLoading || isImageLoading}
+          onClick={(e) => {
+            e.preventDefault();
+            setGenre(genres[Math.floor(genres.length*Math.random())]);
+            setSubject(subjects[Math.floor(subjects.length*Math.random())]);
+            setAccessory(accessories[Math.floor(accessories.length*Math.random())]);
+            setActivity(activities[Math.floor(activities.length*Math.random())]);
+            setLocation(locations[Math.floor(locations.length*Math.random())]);
+          }}
+        >🎲</button>}
+        {genre && subject && <button
+          className="p-4 m-2 border rounded-md shadow-sm font-medium bg-green-300"
+          disabled={isPromptLoading || isImageLoading}
+          onClick={async (e) => {
+            e.preventDefault();
+            try {
+              setPromptLoading(true);
+              const response = await fetch("api/prompt", {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({genre, subject, accessory, activity, location}),
+              });
+              const data = await response.json();
+              setPrompt(data.prompt);
+            } finally {
+              setPromptLoading(false);
+            }
+          }}
+        >{isPromptLoading ? "Generating…" : "Generate prompt"}</button>}
+      </div>
+
+      {genre && subject && prompt && (
+        <div className="rounded-xl">
+          <p><textarea value={prompt} className="w-full h-32 text-black" readOnly></textarea></p>
+        </div>
+      )}
+
+      {/* Dall-E 2 or 3? */}
       {genre && subject && prompt && <select
         id="model"
         name="model"
@@ -131,8 +155,8 @@ export default function Home() {
         className="p-2 m-2 bg-transparent border rounded-md bg-blue-100"
         onChange={(e) => setModel(e.target.value)}
       >
-        <option value="dall-e-2">DALL-E 2</option>
-        <option value="dall-e-3">DALL-E 3</option>
+        <option value="dall-e-2" className="text-black">DALL-E 2</option>
+        <option value="dall-e-3" className="text-black">DALL-E 3</option>
       </select>}
       {genre && subject && prompt && model=="dall-e-3" && <select
         id="quality"
@@ -141,9 +165,11 @@ export default function Home() {
         className="p-2 m-2 bg-transparent border rounded-md bg-blue-100"
         onChange={(e) => setQuality(e.target.value)}
       >
-        <option value="standard">SD</option>
-        <option value="hd">HD</option>
+        <option value="standard" className="text-black">SD</option>
+        <option value="hd" className="text-black">HD</option>
       </select>}
+
+      {/* Extra Dall-E options */}
       {genre && subject && prompt && model=="dall-e-3" && <select
         id="style"
         name="style"
@@ -151,9 +177,12 @@ export default function Home() {
         className="p-2 m-2 bg-transparent border rounded-md bg-blue-100"
         onChange={(e) => setStyle(e.target.value)}
       >
-        <option value="vivid">Vivid</option>
-        <option value="natural">Natural</option>
+        <option value="vivid" className="text-black">Vivid</option>
+        <option value="natural" className="text-black">Natural</option>
       </select>}
+
+      {/* Here would come the prompt */}
+
       {genre && subject && prompt && <button
         className="p-4 m-2 border rounded-md shadow-sm font-medium bg-green-300"
         disabled={isPromptLoading || isImageLoading}
